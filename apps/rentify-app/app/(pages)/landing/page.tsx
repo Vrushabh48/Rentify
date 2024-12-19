2bde6c4c1d2a41b2b6831a24ed6e6c29696b59ac
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-12-19 21:22:13 +0530

    add

---
+++ apps/rentify-app/app/(pages)/landing/page.tsx
@@ -0,0 +1,5 @@
+export default function landing(){
+    return <div>
+        This is the Landing Page
+    </div>
+}
---
+++ apps/rentify-app/app/(pages)/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function profile(){
+    return (
+        <div>
+            <div>
+                <h1>Profile Details</h1>
+            </div>
+        </div>
+    )
+}

8e33999639dcf6b4e99b80601f491087ebbc8444
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2025-01-22 22:55:59 +0530

    removed the code base

--- db/prisma/migrations/20250111125919_added_category_for_product/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `category` to the `Items` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Items" ADD COLUMN     "category" TEXT NOT NULL;
--- db/prisma/migrations/20250113163205_status_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "RentedItem" ALTER COLUMN "approved_status" DROP NOT NULL;
